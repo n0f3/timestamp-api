ca8890cf371ad32301399ded72a1a93a2d9a2ace
Alessandro Metta <n0f3@outlook.com>
2016-08-10 07:27:59 +0000

    Finished app with handling of dates at path"

--- server.js
+++ server.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+
+var htmlDir = path.join(__dirname, 'public', 'html');
 app.set('port', (process.env.PORT || 5000));
-app.get('/', function(req, res) {
-    res.send('Hello World!');
+
+app.get('/', (req, res) => {
+    res.sendFile(htmlDir + '/index.html');
 });
+
+app.get('/:datestring', (req, res) => {
+    var dateParam = req.params.datestring;
+    var objToReturn = {
+        unix: null,
+        natural: null
+    };
+    if(dateParam) {
+        var unixDate = Number(req.params.datestring);
+        var dateObj = null;
+        if(Number.isInteger(unixDate)) {
+            dateObj = new Date(unixDate * 1000);
+            if(dateObj.toString() !== 'Invalid Date') {
+                objToReturn.unix = unixDate;
+                objToReturn.natural = dateObj.toDateString();
+            }
+        } else {
+            dateObj = new Date(dateParam);
+            if(dateObj.toString() !== 'Invalid Date') {
+                objToReturn.unix = dateObj.getTime() / 1000;
+                objToReturn.natural = dateObj.toDateString();
+            }
+        }
+    }
+    res.send(JSON.stringify(objToReturn));
+});
+
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 });
